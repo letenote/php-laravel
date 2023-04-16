bcbd94c76745b6ae2adcdcf9cd00498bcfa7ce8b
Reza Aditya <rezaaditya@192.168.1.12>
2023-04-16 20:29:59 +0700

    add role model and migrate - seed in db

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'name' => 'admin',
+                'redirect_to' => '/administrator',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'user',
+                'redirect_to' => '/',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ];
+
+        foreach ($roles as $role){
+            DB::table('roles')->insert($role);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'name' => 'reza aditya',
+                'email' => 'rezaAditya@gmail.com',
+                'password' => 'admin',
+                'role_id' => 1,
+                'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ];
+
+        foreach ($roles as $role){
+            DB::table('users')->insert($role);
+        }
+    }
+}
