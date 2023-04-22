c4515885f8d91d174a26d922252edcefbcb789bb
Reza Aditya <rezaaditya@192.168.1.6>
2023-04-22 23:27:57 +0700

    add guest and user middleware impl in routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::controller(\App\Http\Controllers\UserController::class) -> group(function (){
-    Route::get('/login', 'login');
-    Route::post('/login', 'doLogin');
-    Route::post('/logout', 'doLogout');
+    Route::get('/login', 'login')->middleware(\App\Http\Middleware\GuestMiddleware::class);
+    Route::post('/login', 'doLogin')->middleware(\App\Http\Middleware\GuestMiddleware::class);;
+    Route::post('/logout', 'doLogout')->middleware(\App\Http\Middleware\UserMiddleware::class);
 });
